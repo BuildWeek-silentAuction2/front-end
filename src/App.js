1dc6d106597c435f37a4e98d9a5606c7465c6bc0
Luis Ocasio <ocasio.dev@gmail.com>
2020-04-29 12:16:21 -0400

    clean up

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import React from 'react';
-import {BrowserRouter as Router, Route} from "react-router-dom";
-
-import Header from './Header';
-import './App.css';
+import React from "react";
+import Header from "./Header";
+import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
-      <Router>
-      </Router>
     </div>
   );
-}
+};
 
 export default App;
 
@@ -23,13 +19,13 @@ export default App;
 // import NewListing from "./components/Seller/NewListing";
 // import NewAuction from "./components/Seller/NewAuction";
 
-{/* <Route exact path="/" component={SellerPage}/>
+/* <Route exact path="/" component={SellerPage}/>
 <Route exact path="/view-auction/:id" component={AuctionPage}/>
 <Route exact path="/new-listing" component={NewListing} />
-<Route exact path="/new-auction" component={NewAuction}/> */}
+<Route exact path="/new-auction" component={NewAuction}/> */
 
 // import BuyerPage from "./components/Buyer/BuyerPage";
 // import AddBid from "./components/Buyer/AddBid";
 
-{/* <Route exact path="/" component={BuyerPage}/>
-<Route exact path="/view-auction/:id" component={AddBid} /> */}
+/* <Route exact path="/" component={BuyerPage}/>
+<Route exact path="/view-auction/:id" component={AddBid} /> */
